4fe7317c0af000c7613fc2364558a9442254c2ef
VladislavMashkov <Vlad_mashkow@mail.ru>
2017-10-09 19:31:38 +0300

    Update 0_0_3.cpp

--- 0_0_3.cpp
+++ 0_0_3.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <list>
+using namespace std;
+
 template <typename Iterator1, typename Iterator2>
 auto merge( Iterator1 first1, Iterator1 last1, Iterator1 first2, Iterator1 last2, Iterator2 output ) {
   while (true) {
@@ -15,19 +17,20 @@ auto merge( Iterator1 first1, Iterator1 last1, Iterator1 first2, Iterator1 last2
 }
 
 int main() {
-  std::vector<int> v1;
+  vector<int> v1;
   v1.push_back(1);
   v1.push_back(7);
 
-  std::list<int> l1;
-  l1.push_back(23);
+  list<int> l1;
+  l1.push_back(0);
   l1.push_back(3);
 
-  std::vector<int> v2( v1.size() + l1.size() );
-  std::vector<int>::iterator first1 = v1.begin(), last1 = v1.end(), output = v2.begin();
-  std::list<int>::iterator first2 = l1.begin(), last2 = l1.end();
+  vector<int> v2( v1.size() + l1.size() );
+  vector<int>::iterator first1 = v1.begin(), last1 = v1.end(), output = v2.begin();
+  list<int>::iterator first2 = l1.begin(), last2 = l1.end();
 
-  merge<std::vector<int>::iterator, std::list<int>::iterator, std::vector<int>::iterator>(first1, last1, first2, last2, output);
+  merge<vector<int>::iterator, list<int>::iterator, vector<int>::iterator>(first1, last1, first2, last2, output);
   for(int i = 0; i < v2.size(); i++)
-        std::cout << v2[i] << ' ';
+        cout << v2[i] << ' ';
+  return 0;
 }
